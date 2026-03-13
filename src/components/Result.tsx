bf77c013ef8bd0f2146055d30c6b98ad9f1485af
Thomas Barrial <thomas@barrial.fr>
2026-03-13 16:18:54 +0100

    uodate

--- src/components/Result.tsx
+++ src/components/Result.tsx
@@ -8,8 +8,12 @@ function Result() {
   const calculatedResult = getSkierDIN(dataSkier);
 
   return (
-    <div className="bg-gray-700 p-5 w-[98%] md:w-full rounded-md space-y-2 flex justify-between items-center shadow-2xl">
-      <h2 className="font-bold text-center md:text-left text-lg">{`RESULT`}</h2>
+    <div className="bg-gray-700 p-4 w-[95%] md:w-full rounded-xl space-y-2 flex justify-between items-center shadow-2xl">
+      <div className="w-10/12">
+        <h2 className="font-bold text-lg">{`RESULT`}</h2>
+        <h3 className="text-xs">Provide your informations to get a result</h3>
+      </div>
+      <p></p>
       <div className="flex justify-between items-end">
         <p className="text-white  text-center md:text-left w-full text-3xl font-bold">
           {calculatedResult}
